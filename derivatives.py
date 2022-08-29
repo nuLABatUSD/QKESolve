5509d1dc16e7ea351bddc3a13cbba2ec190398bf
Delaney Jannone <delaneyjannone@Delaneys-MacBook-Air.local>
2022-08-29 13:50:04 -0700

    finished 2022-3

--- derivatives.py
+++ derivatives.py
@@ -21,12 +21,28 @@ import numba as nb
 
 
 
-@nb.jit(nopython=True)
-def (x, y, p):
-    derivatives = np.zeros(4)
-    derivatives[0] = y[1]
-    derivatives[1] = y[2]
-    derivatives[2] = y[3]
-    derivatives[3] = p**4*y[0]
+#@nb.jit(nopython=True)
+#def f(x, y, p):
+    #derivatives = np.zeros(4)
+    #derivatives[0] = y[1]
+    #derivatives[1] = y[2]
+    #derivatives[2] = y[3]
+    #derivatives[3] = p**4*y[0]
     
+    #return derivatives
+
+
+@nb.jit(nopython=True)
+def f(x, y, p):
+    derivatives = np.zeros(4)
+    derivatives[0] = 0
+    derivatives[1] = p[-1]/(2*p[-3])*(np.cos(2*p[-2])*y[2])
+    derivatives[2] = p[-1]/(2*p[-3])*((-np.cos(2*p[-2])*y[1]) - (np.sin(2*p[-2])*y[3]))
+    derivatives[3] = p[-1]/(2*p[-3])*(np.sin(2*p[-2])*y[2])
+                                           
+                                           
     return derivatives
+                                           
+                                           
+                                           
+                                           
