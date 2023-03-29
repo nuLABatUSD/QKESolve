d46be3259eb45ed40f5d8b2eb9a4b7eaa24def0a
Delaney Jannone <delaneyjannone@Delaneys-MacBook-Air-7.local>
2023-03-29 12:18:39 -0700

    finished 2023-12

--- derivatives.py
+++ derivatives.py
@@ -80,8 +80,9 @@ def f(x,y,p):
         derm= np.zeros(ym.shape)
         Vvv= Vvv_function(ym, energy)
         VT= VT_function(ym, energy, T)
+        Cs= C_scat(C, ym)
         for i in range(derm.shape[0]):
-               derm[i,:]= vacuum(ym[i,:], energy[i], p[-1], p[-2]) + cross_product(ym[i,:], Vvv) + cT*cross_product(ym[i,:], energy[i]*VT) + ym[i, 0]*C[i,:] - C_scat(C, ym)*(1/ym[i,0])
+               derm[i,:]= vacuum(ym[i,:], energy[i], p[-1], p[-2]) + cross_product(ym[i,:], Vvv) + cT*cross_product(ym[i,:], energy[i]*VT) + ym[i, 0]*C[i,:] - Cs[i, :]*(1/ym[i,0])
     
         return array_maker(derm)
     
@@ -99,9 +100,11 @@ def f(x,y,p):
         derm_bar= np.zeros(ym_bar.shape)
         Vvv_Vvvbar= Vvv_function(ym, energy) - Vvv_function(ym_bar, energy)
         VT= VT_barfunction(ym, ym_bar, energy, T)
+        Cs= C_scat(C, ym)
+        Cs_bar= C_scat(D, ym_bar)
         for i in range(derm.shape[0]):
-            derm[i,:]= vacuum(ym[i,:], energy[i], p[-1], p[-2]) + cross_product(ym[i,:], Vvv_Vvvbar) + cT*cross_product(ym[i,:], energy[i]*VT) + ym[i, 0]*C[i,:] - C_scat(C, ym)*(1/ym[i,0])
-            derm_bar[i,:]= -vacuum(ym_bar[i,:], energy[i], p[-1], p[-2]) + cross_product(ym_bar[i,:], Vvv_Vvvbar) - cT*cross_product(ym_bar[i,:], energy[i]*VT) + ym_bar[i, 0]*D[i,:] - C_scat(D, ym_bar)*(1/ym_bar[i,0])
+            derm[i,:]= vacuum(ym[i,:], energy[i], p[-1], p[-2]) + cross_product(ym[i,:], Vvv_Vvvbar) + cT*cross_product(ym[i,:], energy[i]*VT) + ym[i, 0]*C[i,:] - Cs[i, :]*(1/ym[i,0])
+            derm_bar[i,:]= -vacuum(ym_bar[i,:], energy[i], p[-1], p[-2]) + cross_product(ym_bar[i,:], Vvv_Vvvbar) - cT*cross_product(ym_bar[i,:], energy[i]*VT) + ym_bar[i, 0]*D[i,:] - Cs_bar[i, :]*(1/ym_bar[i,0])
         return newarray_maker(derm, derm_bar)
 
 
